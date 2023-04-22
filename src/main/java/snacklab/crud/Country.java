a7d9c71b8310a6cada23c85ab45ef412ccd534d7
volta2030 <volta2030@gmail.com>
2023-04-22 22:42:54 +0900

    -practice sql

---
+++ src/main/java/snacklab/crud/Country.java
@@ -0,0 +1,142 @@
+package snacklab.crud;
+
+public class Country {
+    private String code;
+    private String continent;
+
+    private String Region;
+
+    private double surfaceArea;
+
+    private Object indepYear;
+
+    private int population;
+
+    private Object lifeExpectancy;
+
+    private double gnp;
+
+    private Object gnpOld;
+
+    private String localName;
+
+    private String governmentForm;
+
+    private String headOfState;
+
+    private Object capital;
+
+    private String code2;
+
+    public String getCode() {
+        return code;
+    }
+
+    public void setCode(String code) {
+        this.code = code;
+    }
+
+    public String getCode2() {
+        return code2;
+    }
+
+    public void setCode2(String code2) {
+        this.code2 = code2;
+    }
+
+    public String getContinent() {
+        return continent;
+    }
+
+    public void setContinent(String continent) {
+        this.continent = continent;
+    }
+
+    public String getRegion() {
+        return Region;
+    }
+
+    public void setRegion(String region) {
+        Region = region;
+    }
+
+    public double getSurfaceArea() {
+        return surfaceArea;
+    }
+
+    public void setSurfaceArea(double surfaceArea) {
+        this.surfaceArea = surfaceArea;
+    }
+
+    public Object getIndepYear() {
+        return indepYear;
+    }
+
+    public void setIndepYear(Object indepYear) {
+        this.indepYear = indepYear;
+    }
+
+    public int getPopulation() {
+        return population;
+    }
+
+    public void setPopulation(int population) {
+        this.population = population;
+    }
+
+    public Object getLifeExpectancy() {
+        return lifeExpectancy;
+    }
+
+    public void setLifeExpectancy(Object lifeExpectancy) {
+        this.lifeExpectancy = lifeExpectancy;
+    }
+
+    public double getGnp() {
+        return gnp;
+    }
+
+    public void setGnp(double gnp) {
+        this.gnp = gnp;
+    }
+
+    public Object getGnpOld() {
+        return gnpOld;
+    }
+
+    public void setGnpOld(Object gnpOld) {
+        this.gnpOld = gnpOld;
+    }
+
+    public String getLocalName() {
+        return localName;
+    }
+
+    public void setLocalName(String localName) {
+        this.localName = localName;
+    }
+
+    public String getGovernmentForm() {
+        return governmentForm;
+    }
+
+    public void setGovernmentForm(String governmentForm) {
+        this.governmentForm = governmentForm;
+    }
+
+    public String getHeadOfState() {
+        return headOfState;
+    }
+
+    public void setHeadOfState(String headOfState) {
+        this.headOfState = headOfState;
+    }
+
+    public Object getCapital() {
+        return capital;
+    }
+
+    public void setCapital(Object capital) {
+        this.capital = capital;
+    }
+}
--- src/main/java/snacklab/crud/controller/TestController.java
+++ src/main/java/snacklab/crud/controller/TestController.java
@@ -1,14 +1,37 @@
 package snacklab.crud.controller;
 
+import org.springframework.jdbc.core.BeanPropertyRowMapper;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import snacklab.crud.Country;
+
+import java.util.List;
 
 @Controller
 public class TestController {
+    public List<Country> getCountries() {
+
+        DriverManagerDataSource dataSource = new DriverManagerDataSource();
+        dataSource.setDriverClassName("com.mysql.cj.jdbc.Driver");
+        dataSource.setUrl("jdbc:mysql://localhost:3306/world?serverTimezone=UTC&characterEncoding=UTF-8");
+        dataSource.setUsername("root");
+        dataSource.setPassword("backend1!");
+
+        JdbcTemplate jdbcTemplate = new JdbcTemplate(dataSource);
+        String sql = "SELECT * FROM world.country LIMIT 20";
+        List<Country> countries =  jdbcTemplate.query(sql, new BeanPropertyRowMapper<>(Country.class));
+
+        return countries;
+
+    }
+
     @GetMapping("/test")
     public String index(Model model) {
-        model.addAttribute("name", "SnackLab");
+        model.addAttribute("name", "Load successful!");
+        getCountries();
         return "index";
     }
 }
